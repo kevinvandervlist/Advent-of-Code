3a46a7286386eeaea1bb27d402de545eafb8fb5b
Kevin van der Vlist <kevin@kevinvandervlist.nl>
2020-12-02 09:42:18 +0100

    Day 2

---
+++ puzzles/src/main/scala/nl/kevinvandervlist/aoc2020/day2/PasswordPolicy.scala
@@ -0,0 +1,38 @@
+package nl.kevinvandervlist.aoc2020.day2
+
+object PasswordPolicy {
+
+  def validPasswordsOne(in: List[String]): Int = in
+    .map(passwordEntry)
+    .count(_.isValidOne)
+
+  def validPasswordsTwo(in: List[String]): Int = in
+    .map(passwordEntry)
+    .count(_.isValidTwo)
+
+  private def passwordEntry(line: String): PasswordEntry = {
+    val splitted = line.split(Array(' ', '-', ':'))
+    PasswordEntry(
+      splitted(0).toInt,
+      splitted(1).toInt,
+      splitted(2).head,
+      splitted(4),
+      line
+    )
+  }
+}
+
+private case class PasswordEntry(min: Int, max: Int, letter: Char, entry: String, raw: String) {
+  def isValidOne: Boolean = {
+    val count = entry.count(_ == letter)
+    val lower = count >= min
+    val upper = count <= max
+    lower && upper
+  }
+
+  def isValidTwo: Boolean = {
+    val first = entry(min - 1) == letter
+    val second = entry(max - 1) == letter
+    (first != second) && (first || second)
+  }
+}
---
+++ puzzles/src/test/scala/nl/kevinvandervlist/aoc2020/day2/PasswordPolicySpec.scala
@@ -0,0 +1,35 @@
+package nl.kevinvandervlist.aoc2020.day2
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+import scala.io.Source
+
+class PasswordPolicySpec extends AnyWordSpec with Matchers {
+  private val example =
+    """1-3 a: abcde
+      |1-3 b: cdefg
+      |2-9 c: ccccccccc
+      |""".stripMargin
+
+  private val input = Source.fromResource("day-2-input").getLines().toList
+
+  "Part 1" should {
+    "validate example" in {
+      PasswordPolicy.validPasswordsOne(example.split('\n').toList) shouldBe 2
+    }
+    "validate assignment" in {
+      PasswordPolicy.validPasswordsOne(input) should not be 477
+      PasswordPolicy.validPasswordsOne(input) shouldBe 603
+    }
+  }
+
+  "Part 2" should {
+    "validate example" in {
+      PasswordPolicy.validPasswordsTwo(example.split('\n').toList) shouldBe 1
+    }
+    "validate assignment" in {
+      PasswordPolicy.validPasswordsTwo(input) shouldBe 404
+    }
+  }
+}
