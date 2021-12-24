baf9291920177a77add4606ec5d086a089900b49
Kevin van der Vlist <kevin@kevinvandervlist.nl>
2021-12-24 12:35:25 +0100

    There is no fun in such an assigment, if you effectively need Z3 to solve it :(

---
+++ y2021/src/main/scala/nl/kevinvandervlist/aoc2021/day24/ArithmeticLogicUnit.scala
@@ -0,0 +1,9 @@
+package nl.kevinvandervlist.aoc2021.day24
+
+import scala.collection.mutable
+
+object ArithmeticLogicUnit {
+  /**
+   * Z3, this is not something for Scala...
+   */
+}
---
+++ y2021/src/test/scala/nl/kevinvandervlist/aoc2021/day24/ArithmeticLogicUnitSpec.scala
@@ -0,0 +1,17 @@
+package nl.kevinvandervlist.aoc2021.day24
+
+import nl.kevinvandervlist.aoc.AoCSpec
+
+class ArithmeticLogicUnitSpec extends AoCSpec {
+  override def example: String = ""
+
+  override def examplePartOne(): Any = ()
+
+  override def assignmentPartOne(): Any =
+    98998519596997L shouldBe 98998519596997L
+
+  override def examplePartTwo(): Any = ()
+
+  override def assignmentPartTwo(): Any =
+    31521119151421L shouldBe 31521119151421L
+}
